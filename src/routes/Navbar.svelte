9cb9b8c28df4816b72c07755a618ccf198d29b62
fedfontana <federico.fontana2000@gmail.com>
2023-01-22 00:21:28 +0100

    bigger website name in navbar

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,10 +1,10 @@
 <div class="navbar bg-base-100 p-4">
 	<div class="flex-1">
-		<a class="btn btn-ghost normal-case text-3xl" href="/">gamedex</a>
+		<a class="btn btn-ghost normal-case text-4xl" href="/">gamedex</a>
 	</div>
 	<div class="flex-none gap-2">
 		<div class="form-control">
-			<input type="text" placeholder="Search" class="input input-bordered" />
+			<input type="text" placeholder="Search" class="input input-bordered w-80" />
 		</div>
 		<div class="dropdown dropdown-end">
 			<div class="btn btn-ghost btn-circle avatar">
