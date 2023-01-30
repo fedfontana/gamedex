2b19e194dca05b1680c030bd71c2791a774e20b3
fedfontana <federico.fontana2000@gmail.com>
2023-01-30 23:09:55 +0100

    filter and sorted events by date

--- src/routes/events/+page.server.ts
+++ src/routes/events/+page.server.ts
@@ -1,13 +1,50 @@
 import prisma from "$src/db";
-import { error, type ServerLoad } from "@sveltejs/kit";
+import { error, type Actions, type ServerLoad } from "@sveltejs/kit";
 
-export const load: ServerLoad = async () => {
-    try {
-        const events = await prisma.event.findMany();
+export const load: ServerLoad = async ({ url }) => {
+    const q = url.searchParams.get("q") ?? "";
+    return {
+        events: await get_filtered(q, false),
+    };
+}
+
+export const actions: Actions = {
+    default: async ({ request }) => {
+        const form = await request.formData();
+        const q = (form.get("query") ?? "") as string;
+        const show_all = (form.get("show_all") ?? "off") === "on";
+        console.log("Firing this with q=", q, " and show_all=", show_all);
         return {
-            events
-        };
-    } catch(err) {
-        throw error(500, "Could not fetch events");
+            events: await get_filtered(q, show_all),
+        }   
+    }
+};
+
+async function get_filtered(q: string | undefined, show_all: boolean) {
+    try {
+        const events = await prisma.event.findMany({
+            where: {
+                AND: [
+                    {
+                        name: {
+                            contains: q,
+                        }
+                    },
+                    {
+                        end_dt: show_all ? undefined : {
+                            gte: new Date(),
+                        }
+                    }
+                ]
+            },
+            orderBy: {
+                begin_dt: 'asc',
+            }
+        });
+
+        return events;
+    } catch (err) {
+        console.error("Error while getting events with filters: ", err);
+        throw error(500);
     }
 }
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import { enhance, type SubmitFunction } from '$app/forms';
+	import { page } from '$app/stores';
+	import { addToast } from '$src/toast';
+	import { Search } from 'tabler-icons-svelte';
 	import type { PageData } from './$types';
 	import EventCard from './EventCard.svelte';
 
@@ -9,10 +13,45 @@
 			data.events = data.events.filter((e) => e.id !== id);
 		};
 	}
+
+	const handle_search: SubmitFunction = () => {
+		return async ({ result, form }) => {
+			switch (result.type) {
+				case 'success':
+					data.events = result.data?.events ?? [];
+					form.reset();
+					break;
+				case 'error':
+				case 'failure':
+					addToast({
+						type: 'error',
+						title: 'Something went wrong while fetching events'
+					});
+					break;
+			}
+		};
+	};
 </script>
 
-<div class="flex flex-col gap-6 items-center w-8/12 mx-auto mt-12 mb-24">
-	{#each data.events as event}
-		<EventCard {event} delete_func={delete_with_id(event.id)} />
-	{/each}
+<div class="flex flex-col gap-12 items-center w-8/12 mx-auto mt-12 mb-24">
+	<form method="POST" use:enhance={handle_search} class="flex flex-col gap-2 items-center">
+		<div class="form-control">
+			<div class="input-group">
+				<input type="text" placeholder="Search" name="query" class="input input-bordered w-80" value={$page.url.searchParams.get("q") ?? ''} />
+				<button type="submit" class="btn btn-square btn-primary">
+					<Search class="pr-1" />
+				</button>
+			</div>
+		</div>
+		<span class="flex flex-row gap-2 items-center">
+			<input type="checkbox" name="show_all" class="checkbox checkbox-primary" />
+			<label for="show_all"> show past events </label>
+		</span>
+	</form>
+
+	<div class="flex flex-col gap-6 items-center w-full">
+		{#each data.events as event}
+			<EventCard {event} delete_func={delete_with_id(event.id)} />
+		{/each}
+	</div>
 </div>
